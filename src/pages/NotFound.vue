cb8d6e4d4e4a54a1acc690fcc9a67ede776cad3c
Artem <sqdteffy@gmail.com>
2025-11-26 01:39:36 +0300

    not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>Page is not found!!!!!!</h3>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,34 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
 import Home from "./pages/Home.vue";
 import Users from "./pages/Users.vue";
 import Settings from "./pages/Settings.vue";
 import Todos from "./pages/Todos.vue";
 import Auth from "./pages/Auth.vue";
+import NotFound from "./pages/NotFound.vue";
 
-const UserDetails = () => import('./pages/UserDetails.vue')
+const UserDetails = () => import("./pages/UserDetails.vue");
 
 const routes: RouteRecordRaw[] = [
-  {path: '/', component: Home},
-  {path: '/users', component: Users},
-  {path: '/users/:id', component: UserDetails},
-  {path: '/settings', component: Settings, beforeEnter: (to) => {
-    if (to.query.admin !== '1') return '/'
-  }},
-  {path: '/todos', component: Todos},
-  {path: '/auth', component: Auth}
-]
+  { path: "/", component: Home },
+  { path: "/users", component: Users },
+  { path: "/users/:id", component: UserDetails },
+  {
+    path: "/settings",
+    component: Settings,
+    beforeEnter: (to) => {
+      if (to.query.admin !== "1") return "/";
+    },
+  },
+  { path: "/todos", component: Todos },
+  { path: "/auth", component: Auth },
+  { path: "/:pathMatch(.*)*", component: NotFound },
+];
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
